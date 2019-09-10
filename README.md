a97660f01a4d5ca9ac3fddebfa41daf160fbf1c3
unknown <[shrestha_sl@hotmail.com]>
2019-09-09 22:53:41 -0500

    function added

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# js-functions
-
-1. Chicken Monkey
-2. Battle of the Bands
-3. Cookout
+# Cookout
--- js/battle_bands.js
+++
@@ -1,8 +0,0 @@
-let bandNumber = 0;
-
-function bandSignUp(bandName) {
-  bandNumber++;
-  return bandNumber + " " + bandName;
-}
-console.log(bandSignUp("Beatles"));
-console.log(bandSignUp("Underdogs"));
